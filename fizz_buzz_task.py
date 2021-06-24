e97d954fb503492a599b28ea16e59402e1c7e367
Tom <Tom1@MacBook-Pro-2.local>
2021-06-24 09:27:34 +0100

    fizz_buzz__task file updated

--- fizz_buzz_task.py
+++ fizz_buzz_task.py
@@ -1,15 +1,15 @@
-a = int(input("What number would you like to substitute for Fizz?  "))
+a = int(input("What number would you like to substitute for Fizz?  ")) # prompts user for two inputs in order the substite for Fizz and Buzz
 b = int(input("What number would you like to substitute for Buzz?  "))
 
-for i in range (100):
-    if i % a == 0 and i % b == 0:
-        i = "FizzBuzz"
+for i in range (100): #runs below code loop 100 times
+    if i % a == 0 and i % b == 0: # first checks for muliples of a AND b as cannot check this after i has been changed
+        i = "FizzBuzz" # substitutes for string and prints the string
         print(i)
-    elif i % a == 0:
+    elif i % a == 0: # checks just a
         i = "Fizz"
         print(i)
-    elif i % b == 0:
+    elif i % b == 0: # checks just b
         i = "Buzz"
         print(i)
     else:
-        print(i)
+        print(i) # if no conditions are met it will just print i as the number
